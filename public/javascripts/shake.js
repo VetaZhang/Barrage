16b89c67ee40357754efdacf7871525f754c0ca7
VetaZhang <veta0531@gmail.com>
2014-12-21 11:58:56 +0800

    up

--- public/javascripts/shake.js
+++ public/javascripts/shake.js
@@ -44,6 +44,7 @@ function handler() {
 if (window.DeviceMotionEvent) {
 	window.addEventListener('devicemotion',deviceMotionHandler, false);
 	var loop = setInterval(handler, 300);
+	document.getElementById('debug0').innerHTML = 'loop';
 }
 else {
 	debug.innerHTML = '您的手机不支持加速度感应额～';
